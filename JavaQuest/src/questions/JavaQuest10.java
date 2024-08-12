e7376744d23c9545b2baa242218c0233bcb39972
roy. leung <royleung0406@gmail.com>
2024-08-12 18:01:48 +0800

    12.08.2024

---
+++ JavaQuest/src/questions/JavaQuest10.java
@@ -0,0 +1,50 @@
+package questions;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * Input a Index Position: 3
+ * Input a new Value: 120
+ * Original Array : [25, 14, 56, 15, 36, 56, 77, 18, 29, 49]
+ * New Array: [25, 14, 56, 120, 15, 36, 56, 77, 18, 29]
+ */
+// Insert an elements into a specific position of the array
+// The original last element should be removed accordingly
+// if the specified position is the last index, return the original array
+public class JavaQuest10 {
+
+  public static void main(String[] args) {
+
+    int[] my_array = { 25, 14, 56, 15, 36, 56, 77, 18, 29, 49 };
+
+    Scanner input = new Scanner(System.in);
+
+    System.out.print("Input an index for inserting the value: ");
+    int indexPosition = input.nextInt();
+
+    System.out.print("Input the value: ");
+    int newValue = input.nextInt();
+
+    System.out.println("Original Array : " + Arrays.toString(my_array));
+
+    // code here ...
+    int temp = 0;
+    int arrayTemp =0;
+    temp = my_array[indexPosition];
+    my_array[indexPosition] = newValue;
+    for(int i = indexPosition; i <my_array.length; i++){
+      for(int j = indexPosition; j < my_array.length -1; j++)
+      if(indexPosition < my_array.length -1){
+        arrayTemp = my_array[indexPosition +1];
+        my_array[indexPosition +1] = my_array[indexPosition +2];
+        my_array[indexPosition +2] = arrayTemp;
+      }System.out.println(Arrays.toString(my_array2));
+    }
+    System.out.println(Arrays.toString(my_array));
+
+
+    // my_array = newArr;
+    //System.out.println("New Array: " + Arrays.toString(newArr));
+  }
+}
