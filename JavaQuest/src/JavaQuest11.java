856f7534a2b5ed02283d2980ca665a1e4f3bfb4a
roy. leung <royleung0406@gmail.com>
2024-08-13 17:29:58 +0800

    13.08.2024

---
+++ JavaQuest/src/JavaQuest11.java
@@ -0,0 +1,31 @@
+/**
+ * Input: arrivalTime = 15, delayedTime = 5
+ * Output: 20 
+ * Explanation: Reach at 15+5 = 20 (20:00).
+ *
+ */
+public class JavaQuest11 {
+  // You are given a positive integer arrivalTime denoting the arrival time of a
+  // train in hours,
+  // and another positive integer delayedTime denoting the amount of delay in
+  // hours.
+  // Calculate Delayed Arrival Time
+
+  public static void main(String[] args) {
+    int delayedArrivalTime1 = findDelayedArrivalTime(15, 5); // 20
+    int delayedArrivalTime2 = findDelayedArrivalTime(23, 5); // 4
+    int delayedArrivalTime3 = findDelayedArrivalTime(18, 6); // 0
+    System.out.println(delayedArrivalTime1);
+    System.out.println(delayedArrivalTime2);
+    System.out.println(delayedArrivalTime3);
+  }
+
+  public static int findDelayedArrivalTime(int arrivalTime, int delayedTime) {
+    // code here ...
+    int totalTime = arrivalTime + delayedTime;
+    if (totalTime >= 24){
+      totalTime -= 24;
+    }
+    return totalTime;
+  }
+}
